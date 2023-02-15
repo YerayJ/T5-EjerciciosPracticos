974ab4cd374e1b1f8ddc80daea0023a44e6ae354
YerayJ <yerayjimeenez07@gmail.com>
2023-02-15 20:25:55 +0100

    Terminado juego de cartas

--- src/sieteymedia/Funciones.java
+++ src/sieteymedia/Funciones.java
@@ -1,6 +1,10 @@
 package sieteymedia;
 
+import java.util.Scanner;
+
 public class Funciones {
+    // Creación de escáner para leer datos
+    Scanner key = new Scanner(System.in);
 
     // Array de dos dimensiones que guarda la baraja
     static final double baraja[][] =
@@ -10,21 +14,145 @@ public class Funciones {
                     {1, 2, 3, 4, 5, 6, 7, 0.5, 0.5, 0.5}};
 
     // Contiene el número de la jugada actual
-    int jugadaJ1, jugadaJ2;
+    double jugadaJ1, jugadaJ2;
 
     // El número que introduce el usuario
     String tirada;
 
     // Función que saca una carta con número aleatorio
-    double sacaCarta(){
+    double sacaCarta() {
         // Genera un número aleatorio del 1 al 9
-        int posicion = (int) (Math.random()* baraja[0].length);
-        int tipoCarta = (int) (Math.random()* baraja.length);
+        int posicion = (int) (Math.random() * baraja[0].length);
 
-        double carta = (double) baraja[posicion];
+        // La variable carta coge el valor de la posición en la tabla
+        double carta = baraja[0][posicion];
 
+        // Devuelve la variable
+        return carta;
+    }
 
-        return 2;
+    String sacaTipo() {
+        // Saca un número aleatorio del 0 al 3 que es el número de filas
+        int tipoCarta = (int) (Math.random() * baraja.length);
+
+        // Variable que guarda el nombre del tipo de la carta
+        String tipo = "";
+
+        switch (tipoCarta) {
+            // Según el número que haya sacado se asigna un valor al tipo carta
+            case 0 -> {
+                tipo = "oros";
+            }
+            case 1 -> {
+                tipo = "copas";
+            }
+            case 2 -> {
+                tipo = "espadas";
+            }
+            case 3 -> {
+                tipo = "bastos";
+            }
+        }
+
+        return tipo;
+    }
+
+    double sacaJugada() {
+        // Variable que guarda el valor de la carta actual
+        double carta;
+
+        // Suma de las cartas actual
+        double sumaCartas = 0;
+
+        // Variable que comprueba si sigue jugando
+        boolean sigueJugando = true;
+
+        // Pide tirada al usuario
+        System.out.println("¿Quieres sacar una carta (s/n)? ");
+        tirada = key.next();
+
+        while (tirada.equals("s") && sigueJugando) {
+            // Saca carta y le asigna valor a la variable auxiliar
+            carta = sacaCarta();
+            // La añade a la suma de cartas
+            sumaCartas += carta;
+
+            // Genera una tirada
+            System.out.println("Tu carta es: " + carta + " " + sacaTipo());
+
+            // Muestra puntuación actual
+            System.out.println("Puntuación actual: " + sumaCartas);
+
+            if (sumaCartas <= 7.5) {
+                // Pide tirada al usuario
+                System.out.println("¿Quieres sacar una carta (s/n)? ");
+                tirada = key.next();
+            } else {
+                // Si se pasa del límite la variable cambia a false
+                sigueJugando = false;
+            }
+
+        } // Termina bucle
+
+        return sumaCartas;
+    }
+
+    int compruebaGanador() {
+        // Guarda el resultado. En caso de 0 será empate, 1 gana el jugador uno, 2 gana el jugador dos
+        int esGanador = 0;
+
+        // Calcula la diferencia para saber cuál tiene menos distancia con el resultado
+        double auxJ1 = 7.5 - jugadaJ1;
+        double auxJ2 = 7.5 - jugadaJ2;
+
+        // Si son iguales o se salen los dos del rango es un empate
+        if ((jugadaJ2 == jugadaJ1) || (jugadaJ2 > 7.5 && jugadaJ1 > 7.5) ) {
+
+        } else {
+            // Si el jugador sale del límite gana jugador 1
+            if (jugadaJ1 > 7.5) {
+                esGanador = 2;
+                // Si el jugador sale del límite gana jugador 1
+            } else if (jugadaJ2 > 7.5) {
+                esGanador = 1;
+                // Si la diferencia de J1 es menor ganará él
+            } else if (auxJ1 < auxJ2) {
+                esGanador = 1;
+                // Si la diferencia de J2 es menor ganará él
+            } else if (auxJ1 > auxJ2) {
+                esGanador = 2;
+            }
+
+        }
+
+        // Devuelve la variable que indica quién es ganador
+        return esGanador;
+    }
+
+    boolean repetirPartida() {
+        // Indica si se vuelve a jugar
+        boolean game = false;
+
+        // Guarda decisión de usuario
+        String opc;
+
+        //Pide la opción al usuario
+        opc = key.next();
+
+        // Comprueba si la opción es válida
+        switch (opc) {
+            case "s", "si", "Si", "SI" -> {
+                // Si es si sigue el juego
+                game = true;
+            }
+            case "n", "no", "No", "NO" -> {
+            }
+            case default -> {
+                System.err.println("Esa opción no existe");
+            }
+
+        }
+        return game;
     }
 
 }
--- src/sieteymedia/Game.java
+++ src/sieteymedia/Game.java
@@ -1,6 +1,5 @@
 package sieteymedia;
 
-import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
@@ -8,18 +7,54 @@ public class Game {
         // Objeto para llamar a la clase funciones
         Funciones game = new Funciones();
 
-        // Creación de escáner para leer datos
-        Scanner key = new Scanner(System.in);
+        // Comprueba si el usuario quiere volver a jugar
+        boolean repetirPartida;
 
         do {
-            // Pide tirada al usuario
-            System.out.println("¿Quieres sacar una carta (s/n)? ");
-            game.tirada = key.nextLine();
+            // Empieza el juego
+            System.out.println("Bienvenidos al juego de las Siete y Media");
 
-        // Se ejecuta
-        } while (game.tirada.equals("s"));
+            // Llama a la función para sacar la jugada del jugador 1
+            game.jugadaJ1 = game.sacaJugada();
 
+            // Si el jugador 1 se pasa del rango lo indica pero sigue el juego
+            if (game.jugadaJ1 > 7.5) {
+                System.out.println("Te has pasado");
+            }
+
+            // Empieza el turno del jugador 2
+            System.out.println("EMPIEZA EL TURNO DEL JUGADOR 2");
+
+            // Llama a la función para sacar la jugada del jugador 2
+            game.jugadaJ2 = game.sacaJugada();
+
+            // Si el jugador 1 se pasa del rango lo indica pero sigue el juego
+            if (game.jugadaJ2 > 7.5) {
+                System.out.println("Te has pasado");
+            }
+
+            // Imprime las puntuaciones
+            System.out.println("El JUGADOR 1 tiene " + game.jugadaJ1 + " puntos y el JUGADOR 2 tiene " + game.jugadaJ2 +
+                    " puntos");
+
+            // Comprueba quién es el ganador
+            // Si el valor que devuelve es 0, habrá un empate
+            if (game.compruebaGanador() == 0) {
+                System.out.println("HAN EMPATADO");
+                // En caso de 1 gana el jugador 1
+            } else if (game.compruebaGanador() == 1) {
+                System.out.println("Enhorabuena, JUGADOR 1, has ganado la partida");
+                // Si no se cumplen los anteriores casos gana el jugador 2
+            } else {
+                System.out.println("Enhorabuena, JUGADOR 2, has ganado la partida");
+            }
+
+            // Pregunta si quiere repetir la partida
+            System.out.println("¿Quiere volver a jugar? (s/n)");
+            repetirPartida = game.repetirPartida();
+        } while (repetirPartida);
 
 
     }
+
 }
